f760c1847e22c6fb9d713981854f4bd6bb78519a
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-29 15:55:44 +0200

    first get of articles and messages work with fs.readfile

--- routes/article.js
+++ routes/article.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const articles = require("../articles.json");
 
 app.get("/", (req, res) => {
-  console.log("article route ok");
+  fs.readFile("./articles.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const rawData = data;
+      console.log(rawData);
+      const stringifiedData = rawData.toString();
+      console.log(stringifiedData);
+      const realData = JSON.parse(stringifiedData);
+      console.log(realData);
+
+      res.json(realData);
+    }
+  });
 });
 
 module.exports = app;
--- routes/message.js
+++ routes/message.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const messages = require("../messages.json");
 
 app.get("/", (req, res) => {
-  console.log("message route ok");
+  fs.readFile("./messages.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const rawData = data;
+      console.log(rawData);
+      const stringifiedData = rawData.toString();
+      console.log(stringifiedData);
+      const realData = JSON.parse(stringifiedData);
+      console.log(realData);
+
+      res.json(realData);
+    }
+  });
 });
 
 module.exports = app;
