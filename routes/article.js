bf6733578919ef5c159dd47afe9ef0b962e803d5
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-29 12:30:47 +0200

    fs ok

--- routes/article.js
+++ routes/article.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
 app.get("/", (req, res) => {
-    console.log("article route ok");
+  console.log("article route ok");
 });
 
 module.exports = app;
--- routes/message.js
+++ routes/message.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
 app.get("/", (req, res) => {
   console.log("message route ok");
